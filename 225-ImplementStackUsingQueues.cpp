14e1c6d8ecf2ade57383916ae00ae9a844868adb
Asfia-oishy <asfiamoon126@gmail.com>
2024-09-12 15:31:29 +0600

    accepted

---
+++ 225-ImplementStackUsingQueues.cpp
@@ -0,0 +1,31 @@
+class MyStack {
+public:
+    deque<int> dq;
+
+    MyStack() {}
+
+    void push(int x) { dq.push_back(x); }
+
+    int pop() {
+        int val = dq.back();
+        dq.pop_back();
+        return val;
+    }
+
+    int top() { return dq.back(); }
+
+    bool empty() {
+        if (dq.empty())
+            return true;
+        return false;
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
