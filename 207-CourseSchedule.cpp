eb0705796f8974e2def3d0908f8b88c1db6b91e1
Asfia-oishy <asfiamoon126@gmail.com>
2024-09-20 12:19:20 +0600

    updated

--- 207-CourseSchedule.cpp
+++ 207-CourseSchedule.cpp
@@ -1,56 +1,48 @@
 class Solution {
 public:
-    // Function to add a directed edge to the graph
-    void add_edge(vector<vector<int>>& g, int u, int v) {
-        g[u].push_back(v);
-    }
 
-    // Depth First Search (DFS) function to detect cycles in the graph
-    bool dfs(vector<vector<int>>& g, int src, vector<bool>& vis, vector<bool>& recStack) {
-        vis[src] = true;
-        recStack[src] = true; // Mark the node as part of the current path (recursion stack)
+    bool dfs(int src,vector<vector<int>>& g,vector<int> &vis)
+    {
+        vis[src]=1;
 
-        for (int neighbor : g[src]) {
-            // If the neighbor hasn't been visited, recursively perform DFS
-            if (!vis[neighbor]) {
-                if (dfs(g, neighbor, vis, recStack)) {
-                    return true; // Cycle detected
-                }
+        for(int i=0;i<g[src].size();i++)
+        {
+            int v=g[src][i];
+            if(vis[v]==0)
+            {
+                if(dfs(v,g,vis))
+                 return true;
             }
-            // If the neighbor is in the recursion stack, we found a back edge (cycle)
-            else if (recStack[neighbor]) {
-                return true; // Cycle detected
-            }
+            if(vis[v]==1)
+            return true;
         }
-
-        // Remove the node from the recursion stack once done with DFS for this node
-        recStack[src] = false;
+        vis[src]=2;
         return false;
     }
 
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         int nodes = numCourses;
-        vector<vector<int>> graph(nodes);
+        vector<vector<int>> g(nodes);
 
-        // Build the graph using the prerequisites
-        for (const auto& prereq : prerequisites) {
-            int u = prereq[1]; // Course u is a prerequisite for course v
-            int v = prereq[0];
-            add_edge(graph, u, v);
+        for(int i=0;i<prerequisites.size();i++)
+        {
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+            g[u].push_back(v);
         }
 
-        vector<bool> visited(nodes, false);  // To track visited nodes
-        vector<bool> recStack(nodes, false); // To track nodes in the current recursion stack (path)
+        vector<int> visited(nodes,0);
+        bool cycle = false;
 
-        // Perform DFS for each node (course)
-        for (int i = 0; i < nodes; i++) {
-            if (!visited[i]) {
-                if (dfs(graph, i, visited, recStack)) {
-                    return false; // Cycle detected, cannot finish all courses
-                }
+        for(int i=0;i<nodes;i++)
+        {
+            if(!visited[i])
+            {
+                if(dfs(i,g,visited))
+                cycle = true;
             }
         }
 
-        return true; // No cycle detected, all courses can be finished
+        return !cycle; 
     }
 };
