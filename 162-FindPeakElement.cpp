e892ce7301262b20fb12c16a70e1cc301e372227
Asfia-oishy <asfiamoon126@gmail.com>
2024-09-24 04:32:49 +0600

    updated

--- 162-FindPeakElement.cpp
+++ 162-FindPeakElement.cpp
@@ -12,21 +12,29 @@ public:
         while (l <= h) 
         {
             int mid = l + (h - l) / 2;
-            if(mid==0 && nums[0]>nums[1]) return 0;
-            else if(mid == 0)
+            if(mid==0)
             {
-                l = mid+1;
-                continue;
+                if(nums[0]>nums[1]) return 0;
+                else
+                {
+                    l = mid+1;
+                    continue;
+                }
 
-            }
-
-            if(mid == n-1 && nums[mid]>nums[mid-1]) return mid;
-            else if(mid == n-1)
-            {
-                h=mid-1;
-                continue;
-            }
+            } 
             
+
+            if(mid == n-1)
+            {
+                if(nums[mid]>nums[mid-1]) return mid;
+                else
+                {
+                    h=mid-1;
+                    continue;
+                }
+            } 
+            
+
             if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1])
             {
                 ans = mid;
